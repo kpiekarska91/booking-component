d90310fca0e372d671c3e20cbd35637408dc3200
Katarzyna <kpiekarska91@gmail.com>
2021-01-28 21:37:22 +0100

    Stworzenie komponentu Booking

---
+++ src/components/Booking.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="booking">
+  <h2>Booking</h2>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Booking'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/ViewHome.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <booking></booking>
+  </main>
+</template>
+
+<script>
+
+
+import Booking from './../components/Booking.vue'
+
+export default {
+  name: 'ViewHome',
+
+  components: {
+    Booking
+  },
+
+}
+</script>
+<style scoped>
+
+</style>
